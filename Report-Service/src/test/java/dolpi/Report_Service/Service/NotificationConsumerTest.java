eaef49b8e2abe9d15c5176813fb2e60a9707b632
Manas Rastogi <manasrastogi64@gmail.com>
2026-02-16 13:33:54 +0530

    Update NotificationConsumerTest.java

--- Report-Service/src/test/java/dolpi/Report_Service/Service/NotificationConsumerTest.java
+++ Report-Service/src/test/java/dolpi/Report_Service/Service/NotificationConsumerTest.java
@@ -1,73 +1,73 @@
-package dolpi.Report_Service.Service;
+// package dolpi.Report_Service.Service;
 
-import com.rabbitmq.client.Channel;
-import dolpi.Report_Service.Dto.Notificationmessage;
-import dolpi.Report_Service.Dto.RegisterEntity;
-import dolpi.Report_Service.Dto.RegisterNgo;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.amqp.core.Message;
-import org.springframework.amqp.core.MessageProperties;
+// import com.rabbitmq.client.Channel;
+// import dolpi.Report_Service.Dto.Notificationmessage;
+// import dolpi.Report_Service.Dto.RegisterEntity;
+// import dolpi.Report_Service.Dto.RegisterNgo;
+// import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.extension.ExtendWith;
+// import org.mockito.InjectMocks;
+// import org.mockito.Mock;
+// import org.mockito.junit.jupiter.MockitoExtension;
+// import org.springframework.amqp.core.Message;
+// import org.springframework.amqp.core.MessageProperties;
 
-import java.util.List;
+// import java.util.List;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyBoolean;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.*;
+// import static org.mockito.ArgumentMatchers.any;
+// import static org.mockito.ArgumentMatchers.anyBoolean;
+// import static org.mockito.ArgumentMatchers.anyLong;
+// import static org.mockito.Mockito.*;
 
-@ExtendWith(MockitoExtension.class)
-public class NotificationConsumerTest {
-    @InjectMocks
-    private NotificationConsumer notificationConsumer;
+// @ExtendWith(MockitoExtension.class)
+// public class NotificationConsumerTest {
+//     @InjectMocks
+//     private NotificationConsumer notificationConsumer;
 
-    @Mock
-    private NgoFeignService ngoFeignService;
+//     @Mock
+//     private NgoFeignService ngoFeignService;
 
-    @Mock
-    private MuncipalFeignService muncipalFeignService;
+//     @Mock
+//     private MuncipalFeignService muncipalFeignService;
 
-    @Mock
-    private NotificationFeignService notificationFeignService;
+//     @Mock
+//     private NotificationFeignService notificationFeignService;
 
-    @Mock
-    private Channel channel;
+//     @Mock
+//     private Channel channel;
 
-    @Mock
-    private Message message;
+//     @Mock
+//     private Message message;
 
-    @Test
-    void consume_success_ackCalled() throws Exception {
+//     @Test
+//     void consume_success_ackCalled() throws Exception {
 
-        Notificationmessage msg = new Notificationmessage();
-        msg.setCity("Delhi");
-        msg.setReported_id("123");
+//         Notificationmessage msg = new Notificationmessage();
+//         msg.setCity("Delhi");
+//         msg.setReported_id("123");
 
-        RegisterNgo ngo = new RegisterNgo();
-        ngo.setId("N1");
+//         RegisterNgo ngo = new RegisterNgo();
+//         ngo.setId("N1");
 
-        RegisterEntity entity = new RegisterEntity();
-        entity.setId("M1");
+//         RegisterEntity entity = new RegisterEntity();
+//         entity.setId("M1");
 
-        when(ngoFeignService.findNGO("Delhi"))
-                .thenReturn(List.of(ngo));
+//         when(ngoFeignService.findNGO("Delhi"))
+//                 .thenReturn(List.of(ngo));
 
-        when(muncipalFeignService.findMunicipal("Delhi"))
-                .thenReturn(List.of(entity));
+//         when(muncipalFeignService.findMunicipal("Delhi"))
+//                 .thenReturn(List.of(entity));
 
-        MessageProperties props = new MessageProperties();
-        props.setDeliveryTag(10L);
-        when(message.getMessageProperties()).thenReturn(props);
+//         MessageProperties props = new MessageProperties();
+//         props.setDeliveryTag(10L);
+//         when(message.getMessageProperties()).thenReturn(props);
 
-        notificationConsumer.consume(msg, message, channel);
+//         notificationConsumer.consume(msg, message, channel);
 
-        verify(notificationFeignService, times(2))
-                .savenotifiaction(any());
+//         verify(notificationFeignService, times(2))
+//                 .savenotifiaction(any());
 
-        verify(channel).basicAck(10L, false);
-        verify(channel, never()).basicNack(anyLong(), anyBoolean(), anyBoolean());
-    }
-}
+//         verify(channel).basicAck(10L, false);
+//         verify(channel, never()).basicNack(anyLong(), anyBoolean(), anyBoolean());
+//     }
+// }
